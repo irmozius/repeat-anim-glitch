03e8a789ab27ceea7481af1931d604bb0253f3d9
Kyle <kyz1610@gmail.com>
2023-02-01 01:15:59 +0000

    init

---
+++ anim_rep_test.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var sp = $sp
+@onready var anim = $AnimationPlayer
+@onready var anim_name = $anim_name
+
+var current_anim : String
+var anim_buffer : String
+var anim_active := false
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_up"):
+		if !anim_active:
+			change_anim("going_up")
+		else:
+			buffer_anim("going_up")
+	if Input.is_action_just_pressed("ui_left"):
+		if !anim_active:
+			change_anim("going_left")
+		else:
+			buffer_anim("going_left")
+
+func change_anim(nm):
+	current_anim = nm
+	anim_name.text = nm
+	play_anim(nm)
+
+func play_anim(nm):
+	anim.play(nm)
+	anim_active = true
+	await anim.animation_finished
+	if anim_buffer != "":
+		change_anim(anim_buffer)
+		anim_buffer = ""
+	else:
+		anim_active = false
+		if nm == "idle":
+			return
+		change_anim("idle")
+	
+func buffer_anim(nm):
+	anim_buffer = nm
